5e052194429d4c02ab9d3091467f06662e6ef05a
Felipe Mancilla <felipe.mancilla@alumnos.usm.cl>
2017-10-07 21:11:52 -0300

    Recomendación basado en Rating.

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -25,7 +25,7 @@ class LocationController < ApplicationController
 		if response.code == "200"
       
       @obj = JSON.parse(response.body)
-      #logger.info(@obj['results'][0]['place_id'])
+
       # Obtener el placeID
       place_id = @obj['results'][0]['place_id']
       uri = URI.parse("https://maps.googleapis.com/maps/api/place/details/json?placeid=#{place_id}&key=AIzaSyDqBVFE2MRz8AERZoHM6jqsVZ2f7LMfN8g")
@@ -39,14 +39,100 @@ class LocationController < ApplicationController
         render json: response_details.body
       end
       
-			#render json: response.body
-      #render :index,  status: :ok ,location: @obj
 		end
+  end
+
+  ## Función para recomendar basado en el rating
+
+  def recommendByRating
+
+    #URL que manda latitud,longitud y tag.
+    #Obtener lugares cercanos a un radio definido.
+    uri = URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{locationOwner_params[:location]}&radius=500&type=#{locationOwner_params[:type]}&key=AIzaSyDqBVFE2MRz8AERZoHM6jqsVZ2f7LMfN8g")
+
+    request = Net::HTTP::Get.new(uri)
+    request["X-Riot-Token"] = "RGAPI-ade1178e-06bd-48a2-90fc-1b1a5080ad3c"
+
+    req_options = {
+      use_ssl: uri.scheme == "https",
+    }
+
+    response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+      http.request(request)
+    end
+
+    #Si la consulta se hizo con exito
+    if response.code == "200"
+      
+      #Parsear a JSON
+      @jsonResult = JSON.parse(response.body)
+      list = []
+
+      #Obtener el token para sacar la sgte pagina de resultados.
+      nextPageToken = @jsonResult['next_page_token']
+      list.push(@jsonResult['results']) #Se guardan los resultados en una lista.
+
+      # Google solo muestra 20 resultados minimos y 60 maximos, para acceder a los prox.
+      # 20 resultados, se utiliza el next_page_token.
+      while nextPageToken do        
+          
+        #El tiempo para entregar un token y validarlo, no es rapido (no menos de 1.5 segundo).
+        sleep 1.6
+        uri = URI.parse("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{locationOwner_params[:location]}&radius=500&type=#{locationOwner_params[:type]}&pagetoken=#{nextPageToken}&key=AIzaSyDqBVFE2MRz8AERZoHM6jqsVZ2f7LMfN8g")
+
+        request = Net::HTTP::Get.new(uri)
+        request["X-Riot-Token"] = "RGAPI-ade1178e-06bd-48a2-90fc-1b1a5080ad3c"
+
+        req_options = {
+          use_ssl: uri.scheme == "https",
+        }
+
+        response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+          http.request(request)
+        end
+
+        if response.code == "200"
+
+          @jsonResult = JSON.parse(response.body)
+          list[0]=list[0]+@jsonResult['results']          #Añadir la nueva lista de resultados.
+          nextPageToken = @jsonResult['next_page_token']  #Obtener el next_page_token para la sgte pagina.
+
+        end
+      end
+      
+      list2 = bubble_sort(list[0])    #Ordenar por rating.
+      
+      render json: list2.first(10)    #Entregar los primeros 10 resultados.
+
+    end
+
   end
 
   private
 
+  def bubble_sort(list)
+    return list if list.size <= 1 # ya esta ordenado.
+    swapped = true
+    while swapped do
+      swapped = false
+      0.upto(list.size-2) do |i|
+        number = list[i]["rating"].to_f
+        number2 = list[i+1]["rating"].to_f
+        if number > number2
+          list[i], list[i+1] = list[i+1], list[i] # cambia lugares.
+          swapped = true
+        end
+      end
+    end
+
+    list.reverse
+  end
+
   def location_params
   	params.require(:location).permit(:address)
+  end
+
+  def locationOwner_params
+    params.require(:data).permit(:location,:type)
   end
 end

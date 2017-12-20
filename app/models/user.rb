8c03f872bfb717ece76625d473635639bd08caf3
Franz Wompner <franz.wompner@alumnos.usm.cl>
2017-12-20 11:11:37 -0300

    Change get_place_recommendations method to use MSE

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 require 'mathn'
+
 class User < ApplicationRecord
 	validates :android_id, uniqueness: true
 	has_and_belongs_to_many :preferences
@@ -10,11 +11,14 @@ class User < ApplicationRecord
     ratings = self.ratings
     rating_place_ids = ratings.map(&:place_id)
 
-    # obtener el usuario más similar según distancia coseno
-    v = Vector[*ratings.map {|x| 3 - x.rating }]
-    most_similar_user = User.where("id != ?", self.id).max_by do |user2|
-      v2 = Vector[*user2.ratings.where(place_id: rating_place_ids).map {|x| 3-x.rating }]
-      Math.cos(v.angle_with(v2))
+    # obtener el usuario más similar según menor error cuadrado
+    most_similar_user = User.where("id != ?", self.id).min_by do |user2|
+      u2_ratings = user2.ratings.where(place_id: rating_place_ids)
+      v2 = Vector[*u2_ratings.map(&:rating)]
+      v1 = Vector[*ratings.where(place_id: u2_ratings.map(&:place_id)).map(&:rating)] # get subset
+      p v1, v2
+      # get mean squared error (lol)
+      v1.size == 0 ? Float::INFINITY : (v1-v2).norm
     end
     most_similar_user.ratings.\
       where("place_id NOT IN (?)", rating_place_ids).\

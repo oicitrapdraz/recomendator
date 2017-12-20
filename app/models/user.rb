245f7d18be70aa5836735ec87baf9d60401ebd17
Franz Wompner <franz.wompner@alumnos.usm.cl>
2017-12-20 11:16:47 -0300

    Update get_place_recommendations to use cosine distance again, but fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,14 +11,15 @@ class User < ApplicationRecord
     ratings = self.ratings
     rating_place_ids = ratings.map(&:place_id)
 
-    # obtener el usuario más similar según menor error cuadrado
-    most_similar_user = User.where("id != ?", self.id).min_by do |user2|
+    # obtener el usuario más similar
+    most_similar_user = User.where("id != ?", self.id).max_by do |user2|
       u2_ratings = user2.ratings.where(place_id: rating_place_ids)
       v2 = Vector[*u2_ratings.map(&:rating)]
       v1 = Vector[*ratings.where(place_id: u2_ratings.map(&:place_id)).map(&:rating)] # get subset
       p v1, v2
-      # get mean squared error (lol)
-      v1.size == 0 ? Float::INFINITY : (v1-v2).norm
+
+      #v1.size == 0 ? Float::INFINITY : (v1-v2).norm # mean squared error
+      v1.size == 0 ? 0 : Math.cos(v1.angle_with(v2)) # cosine distance
     end
     most_similar_user.ratings.\
       where("place_id NOT IN (?)", rating_place_ids).\

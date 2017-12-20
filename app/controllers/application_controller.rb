9a73c6f5de4777ccafd94f2997f1d0a4e4208662
Franz Wompner <franz.wompner@alumnos.usm.cl>
2017-12-20 17:08:54 -0300

    UPDATE API KEY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
 	include ActionController::HttpAuthentication::Basic::ControllerMethods
-  	G_API_KEY = "AIzaSyCbz-oDqIOZ9QgEQJmnQfzs1Rlff8YiuMQ"
+  	G_API_KEY = "AIzaSyD-Q728iX5pzaMmtJGrpvfZVnPDfp2ay7U"
+  	#G_API_KEY = "AIzaSyCbz-oDqIOZ9QgEQJmnQfzs1Rlff8YiuMQ"
 end

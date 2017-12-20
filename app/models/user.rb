b1c3b82b59ca219f7b13a1b192abcb2b6160e959
Franz Wompner <franz.wompner@alumnos.usm.cl>
2017-12-20 10:43:02 -0300

    Add place recommender method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,26 @@
+require 'mathn'
 class User < ApplicationRecord
 	validates :android_id, uniqueness: true
 	has_and_belongs_to_many :preferences
   has_many :ratings
 	has_many :places, :through => :ratings
+
+  def get_place_recommendations
+    # sacar ratings del usuario
+    ratings = self.ratings
+    rating_place_ids = ratings.map(&:place_id)
+
+    # obtener el usuario más similar según distancia coseno
+    v = Vector[*ratings.map {|x| 3 - x.rating }]
+    most_similar_user = User.where("id != ?", self.id).max_by do |user2|
+      v2 = Vector[*user2.ratings.where(place_id: rating_place_ids).map {|x| 3-x.rating }]
+      Math.cos(v.angle_with(v2))
+    end
+    most_similar_user.ratings.\
+      where("place_id NOT IN (?)", rating_place_ids).\
+      map {|x| {"name": x.place.name,
+                "place_id": x.place.google_id,
+                "rating": x.rating}}
+  end
+
 end
